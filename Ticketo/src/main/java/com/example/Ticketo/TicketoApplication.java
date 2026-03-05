095d876e53fbc0904a01aa0738392ceb036925e3
Prakhar <prakharshahi9935@gmail.com>
2026-03-06 02:32:12 +0530

    project initialized

---
+++ Ticketo/src/main/java/com/example/Ticketo/TicketoApplication.java
@@ -0,0 +1,14 @@
+package com.example.Ticketo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//entry point of our project
+public class TicketoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketoApplication.class, args);
+	}
+
+}
---
+++ Ticketo/src/test/java/com/example/Ticketo/TicketoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ticketo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
